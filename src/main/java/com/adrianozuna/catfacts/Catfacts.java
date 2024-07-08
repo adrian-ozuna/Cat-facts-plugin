6b81698099338f627df9378a3f7467a1adba97be
Adrian Ozuna <adrian.gab.ozuna@outlook.com>
2024-07-08 06:51:58 -0400

    Added Mr. Hund

--- src/main/java/com/adrianozuna/catfacts/Catfacts.java
+++ src/main/java/com/adrianozuna/catfacts/Catfacts.java
@@ -10,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -19,10 +20,11 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public final class Catfacts extends JavaPlugin implements Listener {
 
-    public String getFact() {
+    public String getCatFact() {
 
         String fact = "";
 
@@ -52,6 +54,49 @@ public final class Catfacts extends JavaPlugin implements Listener {
         return fact;
     }
 
+    public String getDogFact() {
+
+        String fact = "";
+
+        try {
+            // Create a new HttpClient
+            HttpClient client = HttpClient.newHttpClient();
+
+            // Create a GET request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("http://dog-api.kinduff.com/api/facts"))
+                    .GET()
+                    .build();
+
+            // Send the request and get the response
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Parse the response body as JSON
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(response.body());
+
+            JSONArray factsArray = (JSONArray) jsonObject.get("facts");
+
+            // Extract the "fact" field
+            if (!factsArray.isEmpty()) {
+                fact = (String )factsArray.getFirst();
+
+                Bukkit.broadcast(
+                        Component.text("< Mr. Hund > ")
+                                .color(TextColor.color(NamedTextColor.DARK_BLUE))
+                                .append(
+                                        Component.text("Bro will you shut up already? Did YOU know that " + getDogFact())
+                                                .color(TextColor.color(NamedTextColor.WHITE))
+                                )
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return fact;
+    }
+
     @Override
     public void onEnable() {
         BukkitScheduler scheduler = this.getServer().getScheduler();
@@ -65,10 +110,18 @@ public final class Catfacts extends JavaPlugin implements Listener {
                             Component.text("< Mr. Katt > ")
                                     .color(TextColor.color(NamedTextColor.YELLOW))
                                     .append(
-                                            Component.text("Did you know that " + getFact())
+                                            Component.text("Did you know that " + getCatFact())
                                                     .color(TextColor.color(NamedTextColor.WHITE))
                                     )
                     );
+
+                    Random random = new Random();
+
+                    int randomNumber = random.nextInt(100);
+
+                    if (randomNumber < 30) {
+                        getDogFact();
+                    }
                 }
             }
         }, 0L, 18000L);
@@ -83,7 +136,7 @@ public final class Catfacts extends JavaPlugin implements Listener {
         );
 
         event.getPlayer().sendMessage(
-                Component.text("Did you know that " + getFact())
+                Component.text("Did you know that " + getCatFact())
         );
     }
 
